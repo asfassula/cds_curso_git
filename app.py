adc64e0e7f80ed911b005af7a30002bec80e6267
Angelica Fassula <asfassula@gmail.com>
2022-12-12 14:42:14 -0300

    criação de arquivo de carregamento de dados e ajuste de arquivo principal para utilização do novo arquivo criado

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
 
-def load_data():
-    return pd.read.csv ('data/processed/bikes_completed.csv')
+import streamlit as st
+from src.extraction import load_data
+
+st.set_page_config(layout='wide')
 
 def main():
-    df= load_data()
+    df_raw = load_data()
     st.dataframe (df)
 
 if __name__ == "__main__":
     main()
+    
